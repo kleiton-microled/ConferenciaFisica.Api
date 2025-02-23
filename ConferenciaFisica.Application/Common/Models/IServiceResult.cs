d8e5c58e338da0f021c17f9125847d603a7f5773
kleiduda <kleiduda@gmail.com>
2025-02-22 22:34:55 -0300

    Cria o modelo de retorno das requisicoes, ServiceResult

---
+++ ConferenciaFisica.Application/Common/Models/IServiceResult.cs
@@ -0,0 +1,8 @@
+﻿namespace ConferenciaFisica.Application.Common.Models
+{
+    public interface IServiceResult<T>
+    {
+        bool Status { get; set; }
+        IList<string> Mensagens { get; set; }
+    }
+}
